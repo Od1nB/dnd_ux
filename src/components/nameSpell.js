4333160723b014124e1391384a28c19f1689e26e
Odin <odinbjoernebo@gmail.com>
2021-03-13 15:36:28 +0100

    Implemented spellcomponent box and some buttons

--- src/components/nameSpell.js
+++ src/components/nameSpell.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import './../css/spellbox.css'
 
-// function splName(prop){
-//     return(
-//         <h3>name: {prop.name}</h3>
-//     );
-// }
+const url = "https://www.dnd5eapi.co"
+
 
 export default function SplName({ spell }){
+    const [spellData, setSpellData] = useState({});
+
+    useEffect(() => {
+        getSpecific();
+    },[]);
+
+    const getSpecific = async () => {
+        const response = await axios.get(url + spell.url);
+        setSpellData(response.data);
+    };
+    
+
     return(
-        <h4>name: {spell.name}</h4>
+        <div className="spellBox">
+            <h5>{spellData.name}</h5>
+            <p>level: {spellData.level}</p>
+            <p>casting time: {spellData.casting_time}</p>
+        </div>
+
     );
 }
--- src/components/spells.js
+++ src/components/spells.js
@@ -1,15 +1,16 @@
 // import Hode from './header.js';
 import './../css/spells.css';
-import Spellcomp from './spellcomp.js'
-import Navn from './Naaavn.js'
 import { useState , useEffect } from 'react';
 import axios from "axios";
 import SplName from './nameSpell.js'
+import './../css/spellpage/spellcontainer.css'
+import './../css/spellpage/spellbutton.css'
 
-const apilvl1 = 'https://www.dnd5eapi.co/api/spells/?level=5'
+const apilvl1 = 'https://www.dnd5eapi.co/api/spells/?level=3'
+
 
 function SpellsPage() {
-        const [spell2 , setSpe] = useState([]);
+        let [spell2 , setSpe] = useState([]);
 
         useEffect(() => {
             getSpellLVL1();
@@ -19,21 +20,26 @@ function SpellsPage() {
             const response = await axios.get(apilvl1);
             setSpe(response.data.results);
         };
+
+        function buttonClick(){
+            console.log("yeaaa");
+        };
         
         // console.log(spell2);
-        
+        const stats = [0,1,2,3,4,5,6,7,8,9];
+
         return(
             <div className="spellsPage">
-                {/* <Spellcomp/> */}
-                {
-                    spell2.map(spellElem =>(
-                        // <h3>name: {spellElem.name}</h3>
-                        // console.log(spellElem),
-                        <SplName spell = {spellElem}/>
-                    ))
-                }
-                {/* <Navn/> */}
-                {/* <h3>name: {one}</h3> */}
+                <div className="spellButtons">
+                    {stats.map(numb =>(<button onClick={buttonClick}> {numb} </button>))}
+                </div>
+                <div className="spellContainer">
+                    {
+                        spell2.map(spellElem =>(
+                            <SplName spell = {spellElem}/>
+                        ))
+                    }
+                </div>
             </div>
         )
 }
