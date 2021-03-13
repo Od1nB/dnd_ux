3b115ff55916fb214986934ce83a3c70fed82535
TheseusDeus <odinbjoernebo@gmail.vom>
2021-03-13 11:35:45 +0100

    populated spells route with static spells from component and changed some css

---
+++ src/components/nameSpell.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// function splName(prop){
+//     return(
+//         <h3>name: {prop.name}</h3>
+//     );
+// }
+
+export default function SplName({ spell }){
+    return(
+        <h3>name: {spell.name}</h3>
+    );
+}
